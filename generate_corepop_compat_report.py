0d4d26fb291229ecda7605ef9e0d67f20d9a7c6e
Will Price <will.price94@gmail.com>
2021-08-20 00:18:18 +0100

    Generate markdown table

--- generate_corepop_compat_report.py
+++ generate_corepop_compat_report.py
@@ -4,7 +4,7 @@ generate a markdown report of which corepops are compatible with which platforms
 import subprocess
 import functools
 import docker
-from typing import List
+from typing import List, Tuple
 from pathlib import Path
 from concurrent import futures
 
@@ -19,15 +19,20 @@ DEPS = {
 }
 
 CONTAINERS = [
-    ('ubuntu', '16.04'),
-    ('ubuntu', '18.04'),
-    ('ubuntu', '20.04'),
     ('ubuntu', '21.04'),
-    ('fedora', '32'),
-    ('fedora', '33'),
+    ('ubuntu', '20.04'),
+    ('ubuntu', '18.04'),
+    ('ubuntu', '16.04'),
+    ('debian', 'unstable-slim'),
+    ('debian', 'testing-slim'),
+    ('debian', 'stable-slim'),
+    ('debian', 'oldstable-slim'),
+    ('debian', 'oldoldstable'),
     ('fedora', '34'),
-    ('centos', '7'),
+    ('fedora', '33'),
+    ('fedora', '32'),
     ('centos', '8'),
+    ('centos', '7'),
 ]
 
 
@@ -43,34 +48,63 @@ def list_available_corepops() -> List[Path]:
                    if p.name.endswith('.corepop')])
 
 
-def check_corepop_is_valid_in_container(image: str, corepop: Path) -> bool:
-    try:
-        response = DOCKER_CLIENT.containers.run(
-            image=image,
-            command=f"/bin/bash -c '/corepops/{corepop} \":sysexit()\"'",
-            security_opt=["seccomp=unconfined"],
-            volumes={
-                str(Path().absolute()): {'bind': '/corepops', 'mode': 'ro'}
-            }
-        ).decode('utf-8')
-        if len(response.strip()) > 0:
-            return False
-        return True
-    except docker.errors.ContainerError as e:
-        #print(e.container.logs(stdout=True).decode('utf-8'))
-        #print(e.stderr.decode('utf-8'))
-        return False
+def run_corepop_in_container(image: str, corepop: Path) -> Tuple[int, str]:
+    container = DOCKER_CLIENT.containers.run(
+        image=image,
+        command=f"/bin/bash -c '/corepops/{corepop} \":sysexit()\"'",
+        security_opt=["seccomp=unconfined"],
+        volumes={
+            str(Path().absolute()): {'bind': '/corepops', 'mode': 'ro'}
+        },
+        detach=True
+    )
+    exit_code = container.wait()['StatusCode']
+    response = container.logs(stdout=True, stderr=True).decode('utf-8')
+    return exit_code, response
+
+
+def generate_results_table(results):
+    report = """\
+| Corepop | Distribution | Version | Pass | Exit code | Logs |
+| ------- | ------------ | ------- | ---- | --------- | ---- |
+"""
+    for result in results:
+        pass_str = ':heavy_check_mark:' if result['pass'] else ':x:'
+        if len(result['logs'].strip()) == 0:
+            logs_str = ''
+        else:
+            logs_line = result['logs'].replace('\n', '<br>')
+            logs_str = f"<details><summary>details</summary><pre>{logs_line}</pre></details>"
+        report += (
+            f"| {result['core_pop']} | {result['container']} | {result['tag']} " +
+            f"| {pass_str} | {result['exit_code']} | {logs_str} |\n"
+        )
+
+    return report
+
 
 
 def main():
     core_pops: List[Path] = list_available_corepops()
-    configs = [(container, tag, core_pop) for container, tag in CONTAINERS for core_pop in core_pops]
-    with futures.ThreadPoolExecutor(max_workers=10) as executor:
-        results = executor.map(unpack_tuple_args(check_corepop_is_valid_in_container), [
+    configs = [(container, tag, core_pop) for core_pop in core_pops for container, tag in CONTAINERS ]
+    with futures.ThreadPoolExecutor(max_workers=20) as executor:
+        _results = executor.map(unpack_tuple_args(run_corepop_in_container), [
             (f"{container}:{tag}", core_pop) for container, tag, core_pop in configs
         ])
-        for r, (container, tag, corepop) in zip(results, configs):
-            print(f"{container}:{tag}, {corepop}: {r}")
+    results = [
+        {
+            'container': container,
+            'tag': tag,
+            'core_pop': core_pop,
+            'exit_code': exit_code,
+            'logs': logs,
+            'pass': exit_code == 0 and len(logs.strip()) == 0,
+        }
+        for (container, tag, core_pop), (exit_code, logs) in zip(configs, _results)
+    ]
+    table = generate_results_table(results)
+    print(table)
+
 
 if __name__ == '__main__':
     main()
